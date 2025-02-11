dc741c84cb96118acb851e522c51d80d4c97d4fd
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-02-11 21:31:03 +0900

    [Bronze V] Title: 아스키 코드, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/11654. 아스키 코드/아스키 코드.java
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -8,8 +8,7 @@ public class Main{
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char ch = (char) br.read();
-        System.out.println((int) ch);
+        System.out.println(br.read());
         br.close();
     }
 }
