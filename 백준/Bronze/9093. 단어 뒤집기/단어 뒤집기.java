7cf8d953d086c89fa425d442a9de39f9fbc115c9
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-02-17 18:39:33 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 540 ms, Memory: 46756 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.java
@@ -1,15 +1,17 @@
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main{
 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt();
-        scan.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         for (int i = 0; i < n; i++) {
-            String str = scan.nextLine();
+            String str = br.readLine();
             String[] result = str.split(" ");
             StringBuilder sb = new StringBuilder();
             for (String arg : result) {
