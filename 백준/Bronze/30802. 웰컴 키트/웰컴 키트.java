03047fe283e17edfc30d10515dba3bd761bc4cb0
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-02-23 18:34:58 +0900

    [Bronze III] Title: 웰컴 키트, Time: 100 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.java
@@ -8,24 +8,22 @@ public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
         int[] sizes = new int[6];
         for (int i = 0; i < sizes.length; i++) {
             sizes[i] = Integer.parseInt(st.nextToken());
         }
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st2.nextToken());
-        int P = Integer.parseInt(st2.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        int P = Integer.parseInt(st.nextToken());
 
         int totalShirts = 0;
         for (int count : sizes) {
             totalShirts += (count + T - 1) / T;
         }
 
-        int penBundle = N / P;
-        int remain = N % P;
-
-        System.out.println(totalShirts);
-        System.out.print(penBundle + " " + remain);
+        System.out.println(sb.append(totalShirts).append("\n").append(N/P).append(" ").append(N%P));
     }
 }
