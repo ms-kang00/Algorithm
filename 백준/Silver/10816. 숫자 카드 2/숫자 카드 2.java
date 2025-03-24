74dc20487cc946d44b69880ca0e9a2fbc0bf2d26
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-03-24 17:05:44 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 904 ms, Memory: 204640 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -2,63 +2,43 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M;
+    static int N, M, temp;
     
     public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int[] card_plus = new int[10000001];
+        int[] card_minus = new int[10000001];
+
         N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
 
         StringTokenizer st;
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+        for(int i = 0; i < N; i++){
+            temp = Integer.parseInt(st.nextToken());
+            if(temp >= 0)    card_plus[temp]++;
+            else{
+                temp *= -1;
+                card_minus[temp]++;
+            }
         }
 
-        Arrays.sort(arr);
-
         M = Integer.parseInt(br.readLine());
-
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < M; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            int count = maxSearch(arr, num) - minSearch(arr, num);
-            bw.write(count + " ");
+        
+        StringTokenizer aa = new StringTokenizer(br.readLine());
+        for(int i = 0; i < M; i++){
+            temp = Integer.parseInt(aa.nextToken());
+            if(temp >= 0)   bw.write(card_plus[temp] + " ");
+            else{
+                temp *= -1;
+                bw.write(card_minus[temp] + " ");
+            }
         }
         
         br.close();
         bw.flush();
         bw.close();
-    }
-
-    private static int minSearch(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length;
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] >= target) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
-    }
-
-    private static int maxSearch(int[] arr, int target) {
-        int low = 0;
-        int high = arr.length;
-        while (low < high) {
-            int mid = (low + high) / 2;
-            if (arr[mid] > target) {
-                high = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-        return low;
     }
 }
