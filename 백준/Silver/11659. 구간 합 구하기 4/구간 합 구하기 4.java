7827b1604822094b5ad8ff189a0c5f6849a3daf4
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-04-20 10:18:53 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 568 ms, Memory: 61396 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -2,40 +2,37 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n,m;
-    static int i,j;
+    static int n, m;
     static int[] arr;
     static int[] dp;
-    
-    public static void main(String[] args) throws Exception {
-        
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st;
-        st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); // 추가!
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        arr = new int[n+1];
-        dp = new int[n+1];
+        arr = new int[n + 1];
+        dp = new int[n + 1];
 
         String[] nums = br.readLine().split(" ");
         for (int k = 1; k <= n; k++) {
-            arr[k] = Integer.parseInt(nums[k-1]);
-            dp[k] = dp[k-1] + arr[k];
+            arr[k] = Integer.parseInt(nums[k - 1]);
+            dp[k] = dp[k - 1] + arr[k];
         }
-        
 
         for (int k = 0; k < m; k++) {
             st = new StringTokenizer(br.readLine());
-            i = Integer.parseInt(st.nextToken());
-            j = Integer.parseInt(st.nextToken());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
 
-            System.out.println(dp[j] - dp[i-1]);
+            bw.write((dp[j] - dp[i - 1]) + "\n");
         }
-        
-        
-    }
 
+        bw.flush();  // 꼭 버퍼 비우기!
+        bw.close();  // 닫아주기!
+    }
 }
