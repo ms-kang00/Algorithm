befc6d71324f7c6b1db4a1ddca00dca6847a05ee
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-04-27 17:32:36 +0900

    [Silver II] Title: 좌표 압축, Time: 1720 ms, Memory: 190396 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -4,37 +4,36 @@ import java.util.*;
 public class Main {
     static int n, m;
     static int[] map;
-    static Set<Integer> set = new HashSet<>();
-    static Map<Integer, Integer> comp = new HashMap<>();
+    static Set<Integer> set = new TreeSet<>();
     
 
     public static void main(String[] args) throws Exception {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
 
         n = Integer.parseInt(br.readLine());
         map = new int[n];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) {
             m = Integer.parseInt(st.nextToken());
             map[i] = m;
             set.add(m);
         }
         
-        List<Integer> list = new ArrayList<>(set);
-        list.sort(null);
-
-        for (int i = 0; i < list.size(); i++) {
-            comp.put(list.get(i), i);
-        }
-        
-        for (int i : map) {
-            bw.write(comp.get(i) + " ");
+        int[] result = new int[set.size()];
+        int count = 0;
+        for (int i : set) {
+            result[count] = i;
+            count++;
         }
 
-        
+        for (int i = 0; i < n; i++) {
+            int index = Arrays.binarySearch(result, map[i]);
+            bw.write(index + " ");
+        }
 
         bw.flush();
         bw.close();
