d8b0abee1ee17e2e281bdaf32d06a3a62405159b
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-04-27 17:04:41 +0900

    [Silver II] Title: 좌표 압축, Time: 2096 ms, Memory: 279856 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, m;
+    static int[] map;
+    static Set<Integer> set = new HashSet<>();
+    static Map<Integer, Integer> comp = new HashMap<>();
+    
+
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        n = Integer.parseInt(br.readLine());
+        map = new int[n];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            m = Integer.parseInt(st.nextToken());
+            map[i] = m;
+            set.add(m);
+        }
+        
+        List<Integer> list = new ArrayList<>(set);
+        list.sort(null);
+
+        for (int i = 0; i < list.size(); i++) {
+            comp.put(list.get(i), i);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            bw.write(comp.get(map[i]) + " ");
+        }
+
+        
+
+        bw.flush();
+        bw.close();
+    }
+
+}
