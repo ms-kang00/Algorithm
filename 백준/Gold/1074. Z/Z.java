36bc6d15f627abbc555ac72f91910bc5c0169181
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-05-01 13:29:33 +0900

    [Gold V] Title: Z, Time: 120 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int n, r, c;
+    static int result;
+
+    public static void main(String[] args) throws Exception {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+
+        int size = (int)Math.pow(2, n);
+        divide(0, 0, r, c, size);
+
+        bw.write(result + "\n");
+        
+        bw.flush();
+        bw.close();
+    
+    }
+
+    static void divide(int y, int x, int r, int c, int size) {
+        if (size == 1) return;
+
+        int half = size / 2;
+        int block = half * half;
+
+        if (r < y + half && c < x + half) {
+            divide(y, x, r, c, half);
+        } else if (r < y + half && c >= x + half) {
+            result += block;
+            divide(y, x + half, r, c, half);
+        } else if (r >= y + half && c < x + half) {
+            result += block * 2;
+            divide(y + half, x, r, c, half);
+        } else if (r >= y + half && c >= x + half) {
+            result += block * 3;
+            divide(y + half, x + half, r, c, half);
+        }
+    }
+}
