0a075fa9d6efbc2b06d53a3fc742b6c392a8d58b
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-10-20 23:33:09 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 80 ms, Memory: 25,472 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.java
@@ -1,68 +1,59 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Solution
+ {
     static int[][] map;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
 
+        int T = Integer.parseInt(br.readLine());
 
         for (int test_case = 1; test_case <= T; test_case++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int n = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-            map = new int[n][n];
 
-            for (int i = 0; i < n; i++) {
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+
+            map = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < n; j++) {
+                for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
+
             int result = 0;
-
-            for (int i = 0; i < n; i++) {
-                int count = 0;
-                for (int j = 0; j < n; j++) {
-                    if (map[i][j] == 1) {
-                        count++;
-                    } else {
-                        if (count == k) {
-                            result++;
-                        }
-                        count = 0;
-                    }
+            for (int i = 0; i < N; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < N; j++) {
+                    sb.append(map[i][j]);
                 }
-                if (count == k) {
-                    result++;
+
+                String[] parts = sb.toString().split("0");
+                for (String part : parts) {
+                    if (part.length() == K) {
+                        result++;
+                    }
                 }
             }
 
-            for (int j = 0; j < n; j++) {
-                int count = 0;
-                for (int i = 0; i < n; i++) {
-                    if (map[i][j] == 1) {
-                        count++;
-                    } else {
-                        if (count == k) {
-                            result++;
-                        }
-                        count = 0;
+            for (int j = 0; j < N; j++) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < N; i++) {
+                    sb.append(map[i][j]);
+                }
+
+                String[] parts = sb.toString().split("0");
+                for (String part : parts) {
+                    if (part.length() == K) {
+                        result++;
                     }
                 }
-                if (count == k) {
-                    result++;
-                }
             }
-            bw.write("#" + test_case + " " + result + "\n");
-
-
+            System.out.println("#" + test_case + " " + result);
         }
-        bw.flush();
-        bw.close();
     }
-
 }
