614a4acc461cbf7c762ace6112d8c1cea0befa1c
MinSeung Kang <130739202+ms-kang00@users.noreply.github.com>
2025-10-22 18:06:04 +0900

    [D2] Title: 초심자의 회문 검사, Time: 74 ms, Memory: 25,088 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -1,30 +1,31 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class Solution
+ {
 
-
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         int T = Integer.parseInt(br.readLine());
 
-        for(int test_case = 1; test_case <= T; test_case++)
-        {
-            StringBuilder sb = new StringBuilder();
-            String palindrome = br.readLine();
-            char[] arr = palindrome.toCharArray();
-            for (int i = arr.length - 1; i >= 0; i--) {
-                sb.append(arr[i]);
-            }
-            if (palindrome.equals(sb.toString())) {
-                bw.write("#" + test_case + " " + 1 + "\n");
-            } else {
-                bw.write("#" + test_case + " " + 0 + "\n");
+        for (int test_case = 1; test_case <= T; test_case++) {
+            String input = br.readLine();
+
+            int result = 1;
+            int left = 0;
+            int right = input.length() - 1;
+
+            while (left < right) {
+                if (input.charAt(left) != input.charAt(right)) {
+                    result = 0;
+                    break;
+                }
+                left++;
+                right--;
             }
 
+            System.out.println("#" + test_case + " " + result);
         }
-        bw.flush();
-        bw.close();
     }
 }
